96e9c292c35c44cfcc4c45587e187a825456b566
John Boehr <john@johnboehr.is>
2020-05-25 15:31:47 -0700

    Make NixOS 20.03 primary in HerculesCI

--- ci.nix
+++ ci.nix
@@ -16,21 +16,21 @@
 builtins.mapAttrs (k: _v:
   let
     path = builtins.fetchTarball {
-       url = https://github.com/NixOS/nixpkgs/archive/release-19.09.tar.gz;
-       name = "nixpkgs-19.09";
+      url = https://github.com/NixOS/nixpkgs/archive/release-20.03.tar.gz;
+      name = "nixpkgs-20.03";
     };
     pkgs = import (path) { system = k; };
   in
   pkgs.recurseIntoAttrs {
-    n1909 = pkgs.callPackage ./default.nix {};
-
-    n2003 = let
+    n1909 = let
         path = builtins.fetchTarball {
-           url = https://github.com/NixOS/nixpkgs/archive/release-20.03.tar.gz;
-           name = "nixpkgs-20.03";
+          url = https://github.com/NixOS/nixpkgs/archive/release-19.09.tar.gz;
+          name = "nixpkgs-19.09";
         };
         pkgs = import (path) { system = k; };
     in pkgs.callPackage ./default.nix {};
+
+    n2003 = pkgs.callPackage ./default.nix {};
   }
 ) {
   x86_64-linux = {};
